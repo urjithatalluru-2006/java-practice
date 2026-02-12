3a409fef6f012b8806a9cedeea4698f4873276d8
urjithatalluru-2006 <urjithatalluru@gmail.com>
2026-02-12 21:45:16 +0530

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,11 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 4;
+        
+        int result = num1 * num2;
+        
+        System.out.println("Multiplication result: " + result);
+    }
+}

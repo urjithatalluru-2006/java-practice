867b895546265f916ccd38ebc9d16c049f01b21a
urjithatalluru-2006 <urjithatalluru@gmail.com>
2026-02-13 20:08:48 +0530

    Create sumofnumbers.java

---
+++ sumofnumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int num = sc.nextInt();
+            sum += num;
+        }
+
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}

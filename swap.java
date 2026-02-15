31ccafb115fe19a4902d352ccecb5b488126b1f9
urjithatalluru-2006 <urjithatalluru@gmail.com>
2026-02-15 20:13:54 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,19 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
